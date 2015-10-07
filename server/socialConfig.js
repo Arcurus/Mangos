3436a31416173613d783185b531a0575b6d2f170
Samuel Andert <Samuel@Samuels-MacBook-Air.local>
2015-10-07 17:27:12 +0200

    removed socialconfig file

--- server/socialConfig.js
+++ server/socialConfig.js
@@ -4,6 +4,6 @@ ServiceConfiguration.configurations.remove({
 
 ServiceConfiguration.configurations.insert({
   service: "facebook",
-  appId: "969349589794247",
-  secret: "a93c5a6c144c114877a2921d46f53ea6"
+  appId: "1694261200810978",
+  secret: "7771f29b6cd2e6023138bdc122686c62"
 });

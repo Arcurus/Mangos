d83c02790137906be7e1b3f29800b9cc4832096a
Samuel Andert <Samuel@Samuels-MacBook-Air.local>
2015-10-07 18:29:03 +0200

    fixed signup verified At field to 10 years

--- server/accounts.js
+++ server/accounts.js
@@ -9,7 +9,7 @@ Accounts.onCreateUser(function(options, user) {
   user.percent = 0;
   user.mangos = 0;
   user.verified = false;
-  user.verifiedAt = -5;
+  user.verifiedAt = -10;
   return user;
 });
 

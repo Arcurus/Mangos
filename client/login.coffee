b0c78d9ee6ee2a56eb4016db0704943902343bec
Samuel Andert <Samuel@Samuels-MacBook-Air.local>
2015-10-08 18:54:03 +0200

    removed github signup again

--- client/login.coffee
+++ client/login.coffee
@@ -16,10 +16,3 @@ Template.login.events
         error.reason
       else
         Router.go 'home'
-
-  'click .github-login': (event) ->
-    Meteor.loginWithGithub {}, (err) ->
-      if err
-        error.reason
-      else
-        Router.go 'home'
--- server/socialConfig.js
+++ server/socialConfig.js
@@ -7,13 +7,3 @@ ServiceConfiguration.configurations.insert({
   appId: "1694261200810978",
   secret: "7771f29b6cd2e6023138bdc122686c62"
 });
-
-ServiceConfiguration.configurations.remove({
-  service: 'github'
-});
-
-ServiceConfiguration.configurations.insert({
-  service: "github",
-  appId: "2f5e03d3a4de7424a947",
-  secret: "970f2346821a3aa0248da8a4464c32d7216d4005"
-});
